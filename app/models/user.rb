0446129c87aca6c85dcc692c0908f5a81d391e02
rbtgr <rr777robert@gmail>
2019-04-09 01:50:50 +0900

    6.2.4 フォーマットを検証する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+# 参考
+# validates(:name, {presence: true})
+# validates :name, presence: true
+
+  # Emailアドレス検証用の正規表現(ドット連続未対応)
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # ドット連続対応済み
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email,
+    presence: true,
+    length: {maximum: 255},
+    format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  # setupメソッド内に書かれた処理は、各テストが走る直前に実行されます。
+# setupメソッド内に書かれた処理は、各テストが走る直前に実行されます。
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
@@ -14,5 +14,54 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+# 空白の場合は検証でFalseになること
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+# 文字数制限テスト
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+# Emailアドレスチェック
+# 正常系
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+                        #  第二引数はエラーメッセージ
+    end
+  end
+
+# 異常形
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[
+      user@example,com
+      user_at_foo.org
+      user.name@example.
+      foo@bar_baz.com
+      foo@bar+baz.com
+      foo@bar..com
+    ]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 
 end
