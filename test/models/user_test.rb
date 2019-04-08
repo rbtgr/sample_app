faed531b2465a986c7b99aa972f78f661ba0ec53
rbtgr <rr777robert@gmail>
2019-04-09 00:14:10 +0900

    add model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # setupメソッド内に書かれた処理は、各テストが走る直前に実行されます。
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+
 end
