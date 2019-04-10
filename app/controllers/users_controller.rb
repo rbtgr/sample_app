66a3fadc3f74ec02c1232abb10631e27e8f09d49
rbtgr <rr777robert@gmail>
2019-04-10 16:17:51 +0900

    7.1.2 Usersリソース

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    #params[:id]は文字列型の "1" ですが、
+    #findメソッドでは自動的に整数型に変換されます)。
+
+    #debugger
+    #byebug gemによるデバッガー起動
+
+  end
+
   def new
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
 
+  #RESTfulなUsersリソースで必要となるアクションをまとめて追加
+  resources :users
+
 end
