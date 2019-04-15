30c4d76121dc7ff945c39e1801f00d66bf79fc45
rbtgr <rr777robert@gmail>
2019-04-15 11:45:48 +0900

    10.1.1 編集フォーム

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
 #以下はクラスからは呼び出せるが、インスタンスからは呼び出せない
 private
   def user_params # パラメーターのパーミッション設定
