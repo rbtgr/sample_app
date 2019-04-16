0ebb17161720e3e5c0038d0a2d03c342638538a4
rbtgr <rr777robert@gmail>
2019-04-16 19:24:15 +0900

    10.4.2 destroyアクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
  # ログイン済みユーザーかどうか確認
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
  # 正しいユーザーかどうか確認
   before_action :correct_user,   only: [:edit, :update]
    # editとupdateのアクション時、アクションの前にlogged_in_user を実行
@@ -51,6 +51,12 @@ class UsersController < ApplicationController
     else # 失敗
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
 #以下はクラスからは呼び出せるが、インスタンスからは呼び出せない
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 
 User.create!(
   name:  "Example User",
-  email: "example@rails_tutorial.org",
+  email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
   admin: true)
 
 99.times do |n|
 name  = Faker::Name.name
-email = "example-#{n+1}@rails_tutorial.org"
+email = "example-#{n+1}@railstutorial.org"
 password = "password"
 User.create!(name:  name,
     email: email,
