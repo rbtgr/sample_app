0592a0ba91402293e6f3212303fc3f07ab6ecdd9
rbtgr <rr777robert@gmail>
2019-04-16 18:29:56 +0900

    10.3.5 パーシャルのリファクタリング

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,9 +11,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination' , count:2
+
+  # テストデータの出力
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
-      p ""
+      puts
       print (user.id.to_s + "  " )
       print user.name
     end
