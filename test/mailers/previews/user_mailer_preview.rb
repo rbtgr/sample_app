13dcfd488644e56344cbd54fa5f0d8f73ef8c114
rbtgr <rr777robert@gmail>
2019-04-17 13:49:14 +0900

    11.2.1 送信メールのテンプレート 2

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,23 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+# account_activationの引数にはUserオブジェクトが必要なため、
+# user変数を設定し、それをUserMailer.account_activation渡す
+# 同時に アカウント有効化のトークンが必要なので、user.activation_token を設定。
+# activation_tokenは、DBには無い仮の値。
+
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/account_activation
+
   def account_activation
-    UserMailer.account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
     UserMailer.password_reset
   end
-
 end
