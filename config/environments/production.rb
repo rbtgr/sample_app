a0c22b08a2d41a7416b985efdba1529c9199572d
rbtgr <rr777robert@gmail>
2019-04-18 23:01:41 +0900

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # リスト 11.41: Railsのproduction環境でSendGridを使う設定
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://gentle-bastion-36812.herokuapp.com'
+  host = 'gentle-bastion-36812.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
