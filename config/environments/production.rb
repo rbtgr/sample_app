aaee9bc4a67eea863aaa9dc02da9d8e8fb2cc2d6
rbtgr <rr777robert@gmail>
2019-04-18 15:25:02 +0900

    リスト 11.41: Railsのproduction環境でSendGridを使う設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,26 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+
+  # リスト 11.41: Railsのproduction環境でSendGridを使う設定
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  # 注意
+  # user_name や passwordのような重要情報は実値を使わないでENVを利用すること。
+  # そうした変数はSendGridアドオンが自動的に設定してくれますが、
+  # 今回の場合、環境変数を自分で設定しなければなりません。
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
